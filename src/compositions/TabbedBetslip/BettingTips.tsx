bf7604d074ee78f7acd1850e6e6a2d4ab9f0911d
cwisiki <cwisiki@mra.mw>
2025-06-17 00:12:12 +0200

    Betting Tips

--- src/compositions/TabbedBetslip/BettingTips.tsx
+++ src/compositions/TabbedBetslip/BettingTips.tsx
@@ -94,7 +94,9 @@ const BettingTips = () => {
   if (loading) {
     return (
       <div className="flex justify-center items-center h-64">
-        <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-brand-50"></div>
+        <div
+        className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-brand-50"
+        />
       </div>
     )
   }
